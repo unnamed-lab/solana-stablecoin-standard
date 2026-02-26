ba4305c6af1d4f785d49b46433176da696f194c0
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2026-02-26 19:23:35 +0100

    chore: update the backend files and implement sdk

--- backend/libs/blockchain/src/blockchain.module.ts
+++ backend/libs/blockchain/src/blockchain.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { BlockchainService } from './blockchain.service';
 import { ConnectionFactory } from './connection.factory';
+import { SdkService } from './sdk.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [BlockchainService, ConnectionFactory],
-  exports: [BlockchainService, ConnectionFactory],
+  providers: [BlockchainService, ConnectionFactory, SdkService],
+  exports: [BlockchainService, ConnectionFactory, SdkService],
 })
 export class BlockchainModule { }
