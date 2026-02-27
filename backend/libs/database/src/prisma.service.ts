dfaa35e4fb73e51dd63aef26f81fbf8c63a224cc
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2026-02-27 10:26:38 +0100

    fix: improve prisma adapter issues

--- backend/libs/database/src/prisma.service.ts
+++ backend/libs/database/src/prisma.service.ts
@@ -1,11 +1,18 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma/client';
+import { PrismaPg } from "@prisma/adapter-pg";
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+  implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    super({
+      log: ['warn', 'error'],
+      adapter,
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
