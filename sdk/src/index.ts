4e6e8ca3dc22a0d204223f0a21ee77e112ebbe4f
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2026-02-27 04:38:49 +0100

    chore: include IDL in SDK

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -3,3 +3,9 @@ export * from './errors';
 export * from './SolanaStablecoin';
 export * from './modules/compliance';
 export * from './modules/transfer-hook';
+
+// Export IDLs for consumption by clients
+import * as CoreIdl from './idl/sss_core.json';
+import * as TransferHookIdl from './idl/sss_transfer_hook.json';
+
+export { CoreIdl, TransferHookIdl };
